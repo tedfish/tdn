ed16361f4da81ad359b4af69824b957a6b04c138
TDN <tedfish@gmail.com>
2021-06-16 10:48:12 -0700

    working

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {  graphql } from 'gatsby'
-import Img from 'gatsby-image'
+// import Img from 'gatsby-image'
 import { HelmetDatoCms } from 'gatsby-source-datocms'
 import Layout from "../components/layout"
 const IndexPage = ({ data: { home } }) => (
@@ -10,9 +10,12 @@ const IndexPage = ({ data: { home } }) => (
       <div className="sheet__inner">
         <h1 className="sheet__title">{home.title}</h1>
         <p className="sheet__lead">{home.subtitle}</p>
-        <div className="sheet__gallery">
+        <p>
+          <a href="https://docs.google.com/document/d/e/2PACX-1vQP-iP4Ttr1b8yzfwj9cA-XDXFGoXpapcT7_qUsbehupRisxGwBmmv4vWxT73MhB9cXNXZHEm-j86Y6/pub" rel="noreferrer" target="_blank">My résumé</a>  
+        </p>
+        {/* <div className="sheet__gallery">
           <Img fluid={home.photo.fluid} />
-        </div>
+        </div> */}
       </div>
     </article>
   </Layout>
@@ -26,6 +29,7 @@ export const query = graphql`
       }
       title
       subtitle
+      content
       photo {
         fluid(maxWidth: 600, imgixParams: { fm: "jpg", auto: "compress" }) {
           ...GatsbyDatoCmsSizes
