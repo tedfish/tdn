6a4e461176bf2a4c314da576cd1f665bc9c71f2e
TDN <tedfish@gmail.com>
2021-06-24 15:25:27 -0700

    dev

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,29 @@
 import React from 'react'
-import {  graphql } from 'gatsby'
+import { graphql } from 'gatsby'
 import Img from 'gatsby-image'
 import { HelmetDatoCms } from 'gatsby-source-datocms'
 import Layout from "../components/layout"
-const IndexPage = ({ data: { home } }) => (
+const IndexPage = ({ data }) => (
   <Layout>
     <article className="sheet sheet__home">
-      <HelmetDatoCms seo={home.seoMetaTags} />
+      <HelmetDatoCms seo={data.home.seoMetaTags} />
       <div className="sheet__inner">
         <div className="hero_container">
           <div className="sheet__hero">
-            <Img fluid={home.photo.fluid} />
+            <Img fluid={data.home.photo.fluid} />
           </div>
-          <h1 className="sheet__title">{home.title}</h1>
+          <h1 className="sheet__title">{data.home.title}</h1>
         </div>
         <div className="content_container shadow">
-          <p className="sheet__lead">{home.subtitle}</p>
-          <div className="sheet__introText" dangerouslySetInnerHTML={{ __html: home.introText }} />
+          <p className="sheet__lead">{data.home.subtitle}</p>
+          <div className="sheet__introText" dangerouslySetInnerHTML={{ __html: data.home.introText }} />
+        </div>
+        <div class="logos">
+          {data.home.logos.map((item, i) => (
+              <div key={i}>
+                <Img fluid={item.fluid}/>
+              </div>
+            ))}
         </div>
       </div>
     </article>
@@ -32,6 +39,12 @@ export const query = graphql`
       title
       subtitle
       introText
+      logos {
+        url
+        fluid(maxWidth: 200, imgixParams: { fm: "jpg", auto: "compress" }) {
+          ...GatsbyDatoCmsSizes
+        }
+      }
       content
       photo {
         fluid(maxWidth: 2000, imgixParams: { fm: "jpg", auto: "compress" }) {
